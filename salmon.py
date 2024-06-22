699ab8b93b84df8f992295a27eb8c8a8c26806fe
root <Solomon970>
2024-06-22 21:59:35 +0000

    salmon commit

---
+++ salmon.py
@@ -0,0 +1,39 @@
+
+
+#string = "hello world!"
+
+#substring = string[6]
+#print(dir(string))
+
+#name = "salmon"
+#print("Your name is", name)
+#print("Your name is", name, end='_')
+
+#print(string[0.5].upper(), string[6:].lower())
+
+
+#number = int(input("please enter a number:"))
+#x = 5 
+
+#if x > number:
+  
+#else
+#print(x, "is less than:", number)
+
+
+name = input("please enter your name:")
+
+age = input("Enter your age:")
+
+age_limmit = 21
+
+if name == "Salmon" and float(age) >= age_limit:
+
+    print ("You're welcome to drink", name)
+
+elif name == "simon":
+
+      print("You are banned", name)
+
+else: 
+    print("You are too young")
