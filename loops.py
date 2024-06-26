4376aaa4509dda7128a4f5f7cff98e3117daae22
root <Solomon970>
2024-06-26 01:52:19 +0000

    i am good

---
+++ loops.py
@@ -0,0 +1,63 @@
+#x = 0 
+#while x <= 5:
+       # x += 1 
+        #print(x)
+
+
+#for i in range(5, 10):
+    #print(i)
+
+    #x = 0 
+    #if x <0:
+        #pass
+    #else 
+
+    #cities = ['New york', 'Los angeles', 'Chicago', 'Huston', 'Boston',]
+
+    #print(cities)
+
+   #cities.append("phoenix")
+   #print(cities)
+
+
+#cities = ['New york', 'Los angeles', 'Chicago', 'Huston', 'Boston',]
+#for index in range(len(cities)):
+# print("current index: ", '| current city: ', cities[index])
+
+# set a empty list 
+# while the lenght of the list is less thna 5
+#  ask user to enter a city name 
+# befoer appending the city to the list check if city exists
+
+#cities = []
+
+#while len(cities) <= 5:
+    # user_input = input("please enter city name:")
+    #if user_input in cities:
+        #print("city already exists:", user_input)
+           # break 
+        #else:
+            #cities.append(user_input)
+
+           # print(len(cities), cities)
+
+
+#spendings = [32.45, 18.65, 23.45, 78.32, 5.23]
+
+#sum = 0.0
+#for spending in spendings:
+    #sum += spending
+#print('Money spent:', sum)
+
+#spendings.sort(reverse=True)
+#print(spendings)
+
+#words = ["banana", "apple", "cherry", "acute", "exit", "Abdul"]
+#words.sort()
+#print(words)  # Output: ['apple', 'banana', 'cherry']
+
+##original_list = [1, 2, 3, 4, 5]
+#3new_list = original_list.copy()
+#new_list.append(6)
+#print(new_list)
+#print(original_list) 
